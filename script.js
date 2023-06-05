4de44a742434801f0f9e16eeda1d8ad0cabcf6c5
Supriya Limbole <127923605+BoonDocks-sl@users.noreply.github.com>
2023-06-05 20:22:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // your code here
+// Get the form and the h3 element
+const form = document.querySelector('#urlForm');
+const urlH3 = document.querySelector('#url');
+
+// Handle form submission
+form.addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent default form submission
+  
+  // Get the input values
+  const name = document.querySelector('#name').value;
+  const year = document.querySelector('#year').value;
+  
+  // Update the query string in the h3 element
+  urlH3.textContent = `https://localhost:8080/?name=${name}&year=${year}`;
+  
+  // Reset the form
+  form.reset();
+});
